27d59e3abef241272f439e318303c9fbb4a6ddbd
rafaelgreca <rgvieira97@gmail.com>
2022-07-27 19:51:46 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+from src.utils import download_files, download_labels_file
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="AVA-Speech dataset downloader")
+    parser.add_argument("-labels_file", "--labels_file", type=str, help="Labels file directory (str)")
+    parser.add_argument("-fs", "--fs", type=int, help="Sample rate in Hz (int)")
+    parser.add_argument("-o", "--o", type=str, help="Output directory (str)")
+    parser.add_argument("-multiprocessing", "--multiprocessing", action='store_true', dest='multiprocessing', help="Use multiprocessing (bool)")
+    parser.add_argument("-overwrite", "--overwrite", action='store_true', dest='overwrite', help="Overwrite existing files (bool)")
+    parser.set_defaults(
+        fs=16000,
+        multiprocessing=False,
+        overwrite=False,
+        o=os.path.join(os.getcwd(), "dataset"),
+        labels_file=None
+    )
+    args = parser.parse_args()
+    
+    assert args.fs in [8000, 16000, 44000]
+
+    if args.labels_file == None:
+        download_labels_file()
+        labels_files_dir = os.getcwd()
+    else:
+        assert os.path.exists(args.labels_file)
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+def download_labels_file():
+    os.system("wget -c https://research.google.com/ava/download/ava_speech_labels_v1.csv -q")
+
+def create_files_csv(input_files: str):
+    pass
+
+def download_files(input_files: str):
+    pass

5295f7f131cd559e27dae8471cfc53ade152b398
rafaelgreca <rgvieira97@gmail.com>
2022-07-29 20:18:51 -0300

    [Feature] Code improved + more features

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import argparse
 import os
+import pandas as pd
 from src.core import download_files, download_labels_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AVA-Speech dataset downloader")
     parser.add_argument("-labels_file", "--labels_file", type=str, help="Labels file directory (str)")
-    parser.add_argument("-fs", "--fs", type=int, help="Sample rate in Hz (int)")
-    parser.add_argument("-o", "--o", type=str, help="Output directory (str)")
-    parser.add_argument("-multiprocessing", "--multiprocessing", action='store_true', dest='multiprocessing', help="Use multiprocessing (bool)")
-    parser.add_argument("-overwrite", "--overwrite", action='store_true', dest='overwrite', help="Overwrite existing files (bool)")
+    parser.add_argument("-fs", "--fs", type=int, help="Sample rate in Hz (int). Default: 16000")
+    parser.add_argument("-o", "--o", type=str, help="Output directory (str). Default: root_directory_folder/dataset")
+    parser.add_argument("-multiprocessing", "--multiprocessing", action='store_true', dest='multiprocessing', help="Use multiprocessing (bool). Default: False")
+    parser.add_argument("-overwrite", "--overwrite", action='store_true', dest='overwrite', help="Overwrite existing files (bool). Default: False")
+    parser.add_argument("-c", "--c", nargs="+", type=str.lower, help="Specify the classes you want to download (str). Default: all")    
+    parser.add_argument("-max_files", "--max_files", type=int, help="Max files to download from each class (int). Default: None (all of them)")
     parser.set_defaults(
         fs=16000,
         multiprocessing=False,
         overwrite=False,
         o=os.path.join(os.getcwd(), "dataset"),
-        labels_file=None
+        labels_file=None,
+        c="all",
+        max_files=None
     )
     args = parser.parse_args()
     
-    assert args.fs in [8000, 16000, 44000]
+    all_classes = ["clean-speech", "speech-noise", "speech-music", "no-speech"]
+    available_fs = [8000, 16000]
+    map_classes = {
+        "clean-speech": "CLEAN_SPEECH",
+        "no-speech": "NO_SPEECH",
+        "speech-noise": "SPEECH_WITH_NOISE",
+        "speech-music": "SPEECH_WITH_MUSIC"
+    }
+
+    assert args.fs in available_fs, f"The fs must be {available_fs}"
+    assert all([c in all_classes for c in args.c]), f"The class(es) must be {all_classes}"
+    assert ((args.max_files == None) or ((type(args.max_files) == int) and (args.max_files > 0))), f"You must provide a valid value for max_files or let it empty to download everything"
+
+    if args.c != "all":
+        all_classes = [map_classes[c] for c in args.c]
+    else:
+        all_classes = [map_classes[c] for c in all_classes]
+
     os.makedirs(args.o, exist_ok=True)
 
     if args.labels_file == None:
@@ -27,7 +49,13 @@ if __name__ == "__main__":
     else:
         assert os.path.exists(args.labels_file)
     
-    download_files(input_files=os.path.join(os.getcwd(), "ava_speech_labels_v1.csv"),
+    df = pd.read_csv(os.path.join(os.getcwd(), "ava_speech_labels_v1.csv"), sep=",", header=None)
+    df = df[df[3].isin(all_classes)]
+    
+    download_files(df=df,
                    use_multiprocessing=args.multiprocessing,
                    output_path=args.o,
-                   fs=args.fs)
+                   fs=args.fs,
+                   max_files=args.max_files,
+                   classes=all_classes,
+                   overwrite=args.overwrite)
--- src/core.py
+++ src/core.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 import os
 import pandas as pd
 import multiprocessing
@@ -13,22 +14,40 @@ def _aux_download_file(video_id, start_timestamp, end_timestamp, label, output_p
     ffmpeg_command = f"ffmpeg -ss {start_timestamp} -to {end_timestamp} -i $({youtube_dl_command}) -ar {fs} -hide_banner -v warning {output_path}/{video_id}-{start_timestamp}-{end_timestamp}-{label}.wav"
     os.system(ffmpeg_command)
 
-def download_files(input_files: str,
+def download_files(df: pd.DataFrame,
                    use_multiprocessing: bool,
                    output_path: str,
-                   fs: int):
+                   fs: int,
+                   max_files: Union[None, int],
+                   classes: list,
+                   overwrite: bool):
 
-    df = pd.read_csv(input_files, sep=',', header=None)
+    if max_files != None:
+        count_classes = {c:0 for c in classes}
 
     if use_multiprocessing:
         num_workers = multiprocessing.cpu_count() - 1
         pool = multiprocessing.Pool(num_workers)
 
-        for infos in zip(df[0], df[1], df[2], df[3], [output_path]*len(df[0]), [fs]*len(df[0])):
-            pool.starmap_async(_aux_download_file, [infos])
+        # d[0] = youtube video id
+        # d[1] = start timestamp
+        # d[2] = end timestamp
+        # d[3] = label
+        for i, infos in enumerate(zip(df[0], df[1], df[2], df[3], [output_path]*len(df[0]), [fs]*len(df[0]))):
+            if count_classes[df.iloc[i, 3]] < max_files:                
+                pool.starmap_async(_aux_download_file, [infos])
+                count_classes[df.iloc[i, 3]] += 1
+            else:
+                is_over = all([count_classes[c] > max_files for c in count_classes.keys()])
+                if is_over: break;
         
         pool.close()
         pool.join()
     else:
-        for video_id, start_timestamp, end_timestamp, label in zip(df[0], df[1], df[2], df[3]):
-            _aux_download_file(video_id, start_timestamp, end_timestamp, label, output_path, fs)
+        for i, (video_id, start_timestamp, end_timestamp, label) in enumerate(zip(df[0], df[1], df[2], df[3])):
+            if count_classes[df.iloc[i, 3]] < max_files:           
+                _aux_download_file(video_id, start_timestamp, end_timestamp, label, output_path, fs)
+                count_classes[df.iloc[i, 3]] += 1
+            else:
+                is_over = all([count_classes[c] >= max_files for c in count_classes.keys()])
+                if is_over: break

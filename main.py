a895fe1ddca1f89c18afe8d03fbf7654d63365ca
rafaelgreca <rgvieira97@gmail.com>
2022-07-30 12:07:21 -0300

    [FIX] Fix some minor issues and comments added

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from src.core import download_files, download_labels_file
+
 import argparse
 import os
 import pandas as pd
-from src.core import download_files, download_labels_file
+import logging
+
+## create the log file
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("./logging.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AVA-Speech dataset downloader")
     parser.add_argument("-labels_file", "--labels_file", type=str, help="Labels file directory (str)")
     parser.add_argument("-fs", "--fs", type=int, help="Sample rate in Hz (int). Default: 16000")
     parser.add_argument("-o", "--o", type=str, help="Output directory (str). Default: root_directory_folder/dataset")
-    parser.add_argument("-multiprocessing", "--multiprocessing", action='store_true', dest='multiprocessing', help="Use multiprocessing (bool). Default: False")
-    parser.add_argument("-overwrite", "--overwrite", action='store_true', dest='overwrite', help="Overwrite existing files (bool). Default: False")
+    parser.add_argument("-multiprocessing", "--multiprocessing", action="store_true", dest="multiprocessing", help="Use multiprocessing (bool). Default: False")
+    parser.add_argument("-overwrite", "--overwrite", action="store_true", dest="overwrite", help="Overwrite existing files (bool). Default: False")
     parser.add_argument("-c", "--c", nargs="+", type=str.lower, help="Specify the classes you want to download (str). Default: all")    
     parser.add_argument("-max_files", "--max_files", type=int, help="Max files to download from each class (int). Default: None (all of them)")
     parser.set_defaults(
@@ -36,22 +48,33 @@ if __name__ == "__main__":
     assert all([c in all_classes for c in args.c]), f"The class(es) must be {all_classes}"
     assert ((args.max_files == None) or ((type(args.max_files) == int) and (args.max_files > 0))), f"You must provide a valid value for max_files or let it empty to download everything"
 
+    ## mapping the chosen classes
     if args.c != "all":
         all_classes = [map_classes[c] for c in args.c]
     else:
         all_classes = [map_classes[c] for c in all_classes]
 
+    ## creating the output path folder if doesn't exists already
     os.makedirs(args.o, exist_ok=True)
 
+    ## if the labels_file path is None, then download the main csv file
     if args.labels_file == None:
         download_labels_file()
-        labels_files_dir = os.getcwd()
+        labels_files_dir = os.path.join(os.getcwd(), "ava_speech_labels_v1.csv")
     else:
+        ## check if the labels_file path exists
         assert os.path.exists(args.labels_file)
+
+        ## check if the main csv is in the labels_file path
+        assert os.path.exists(os.path.join(args.labels_file, "ava_speech_labels_v1.csv")), f"You need to download the ava_speech_labels_v1 csv file or keep the labels_file param None"
     
-    df = pd.read_csv(os.path.join(os.getcwd(), "ava_speech_labels_v1.csv"), sep=",", header=None)
+        labels_files_dir = os.path.join(args.labels_file, "ava_speech_labels_v1.csv")
+
+    ## filter the classes to keep only what we want to download
+    df = pd.read_csv(labels_files_dir, sep=",", header=None)
     df = df[df[3].isin(all_classes)]
     
+    ## download the files
     download_files(df=df,
                    use_multiprocessing=args.multiprocessing,
                    output_path=args.o,
--- src/core.py
+++ src/core.py
@@ -1,16 +1,38 @@
 from typing import List, Union
+
 import os
 import pandas as pd
 import multiprocessing
 
-OVERWRITE_FILE = 'n' # False
+def download_labels_file() -> None:
+    """
+    If the main csv file isn't in the root path, then download it.
 
-def download_labels_file():
+    Param:
+        None
+    Return:
+        None
+    """
     os.system("wget -c https://research.google.com/ava/download/ava_speech_labels_v1.csv -q")
 
-def _aux_download_file(video_id, start_timestamp, end_timestamp, label, output_path, fs):
+def _aux_download_file(video_id: str,
+                       start_timestamp: float,
+                       end_timestamp: float,
+                       label: str) -> None:
+    """
+    Auxiliar function to download the videos.
+
+    Param:
+        :video_id: The YouTube's video id.
+        :start_timestamp: The beggining of the video segment.
+        :end_timestamp: The end of the video segment.
+        :label: The corresponding label to that video segment.
+    Return:
+        None
+    """
     youtube_dl_command = f"youtube-dl --format 'bestaudio' --get-url https://www.youtube.com/watch?v={video_id}"
-    ffmpeg_command = f"ffmpeg -{OVERWRITE_FILE} -ss {start_timestamp} -to {end_timestamp} -i $({youtube_dl_command}) -ar {fs} -hide_banner -v warning {output_path}/{video_id}-{start_timestamp}-{end_timestamp}-{label}.wav"
+    ffmpeg_command = f"ffmpeg -{OVERWRITE_FILE} -ss {start_timestamp} -to {end_timestamp} -i $({youtube_dl_command}) " + \
+                     f"-ar {FRAME_SAMPLE} -hide_banner -v warning {PATH_OUTPUT}/{video_id}-{start_timestamp}-{end_timestamp}-{label}.wav"
     os.system(ffmpeg_command)
 
 def download_files(df: pd.DataFrame,
@@ -19,11 +41,36 @@ def download_files(df: pd.DataFrame,
                    fs: int,
                    max_files: Union[None, int],
                    classes: List,
-                   overwrite: bool):
+                   overwrite: bool) -> None:
+    """
+    Main function responsible to download the videos.
+
+    Param:
+        :df: The csv containing 
+        :use_multiprocessing: Use multiprocessing or not.
+        :output_path: Path where the downloaded videos will be saved.
+        :fs: Frame sample of the video.
+        :max_files: Max files to be downloaded for each class.
+        :classes: Class(es) that will be downloaded.
+        :overwrite: Overwrite or not the files (if already exists).
+    Return:
+        None
+    """
+    global OVERWRITE_FILE
+    global FRAME_SAMPLE
+    global PATH_OUTPUT
 
     if overwrite:
-        OVERWRITE_FILE = 'y' # True
+        OVERWRITE_FILE = 'y'
+    else:
+        OVERWRITE_FILE = 'n'
+    
+    FRAME_SAMPLE = fs
+    PATH_OUTPUT = output_path
 
+    ## if max_files param isn't none, create a dictionary
+    ## to keep track how many files have already been downloaded
+    ## from each class
     if max_files != None:
         count_classes = {c:0 for c in classes}
 
@@ -35,21 +82,34 @@ def download_files(df: pd.DataFrame,
         # d[1] = start timestamp
         # d[2] = end timestamp
         # d[3] = label
-        for i, infos in enumerate(zip(df[0], df[1], df[2], df[3], [output_path]*len(df[0]), [fs]*len(df[0]))):
-            if count_classes[df.iloc[i, 3]] < max_files:                
-                pool.starmap_async(_aux_download_file, [infos])
-                count_classes[df.iloc[i, 3]] += 1
+        for i, infos in enumerate(zip(df[0], df[1], df[2], df[3])):
+
+            if max_files != None:
+                ## check if we have passed the max files amount for that class
+                if count_classes[df.iloc[i, 3]] < max_files:                
+                    pool.starmap_async(_aux_download_file, [infos])
+                    count_classes[df.iloc[i, 3]] += 1
+                else:
+                    ## check if we have passed the max files amount for all the classes
+                    is_over = all([count_classes[c] > max_files for c in count_classes.keys()])
+                    if is_over: break;
             else:
-                is_over = all([count_classes[c] > max_files for c in count_classes.keys()])
-                if is_over: break;
-        
+                pool.starmap_async(_aux_download_file, [infos])
+
         pool.close()
         pool.join()
+
     else:
         for i, (video_id, start_timestamp, end_timestamp, label) in enumerate(zip(df[0], df[1], df[2], df[3])):
-            if count_classes[df.iloc[i, 3]] < max_files:           
-                _aux_download_file(video_id, start_timestamp, end_timestamp, label, output_path, fs)
-                count_classes[df.iloc[i, 3]] += 1
+            
+            if max_files != None:
+                ## check if we have passed the max files amount for that class
+                if count_classes[df.iloc[i, 3]] < max_files:           
+                    _aux_download_file(video_id, start_timestamp, end_timestamp, label)
+                    count_classes[df.iloc[i, 3]] += 1
+                else:
+                    ## check if we have passed the max files amount for all the classes
+                    is_over = all([count_classes[c] >= max_files for c in count_classes.keys()])
+                    if is_over: break
             else:
-                is_over = all([count_classes[c] >= max_files for c in count_classes.keys()])
-                if is_over: break
+                _aux_download_file(video_id, start_timestamp, end_timestamp, label)
